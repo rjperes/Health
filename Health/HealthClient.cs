bb33b805606a1d6d5cfa5c737a1aa841cd0c64c9
Ricardo Peres <rjperes@hotmail.com>
2024-07-25 17:56:47 +0100

    Improvements

---
+++ Health/HealthClient.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Net;
+
+namespace Health
+{
+    public interface IHealthClient
+    {
+        Task<HealthStatus> IsHealthy(string url, CancellationToken cancellationToken = default);
+    }
+
+    public class HealthClient : IHealthClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public HealthClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<HealthStatus> IsHealthy(string url = "/Health", CancellationToken cancellationToken = default)
+        {
+            var response = await _httpClient.GetAsync(url, cancellationToken);
+            return response.StatusCode == HttpStatusCode.ServiceUnavailable ? HealthStatus.Unhealthy : HealthStatus.Healthy;
+        }
+    }
+}
---
+++ Health/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Net.Http.Headers;
+using System.Net.Mime;
+
+namespace Health
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddHealthClient(this IServiceCollection services, string baseAddress)
+        {
+            services.AddHttpClient("HealthClient", client =>
+            {
+                client.BaseAddress = new Uri(baseAddress);
+                client.DefaultRequestHeaders.Add(HeaderNames.Accept, MediaTypeNames.Application.Json);
+                client.DefaultRequestHeaders.Add(HeaderNames.CacheControl, "no-cache");
+            }).AddTypedClient<IHealthClient, HealthClient>();
+
+            return services;
+        }
+    }
+}
